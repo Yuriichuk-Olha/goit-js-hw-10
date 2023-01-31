4f777fd98537496e2b3c4c784f0ab7dabe0c6cbd
Yuriichuk-Olha <olga.kantemyr@gmail.com>
2023-02-01 00:29:45 +0100

    delete then and catch

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,16 +1,9 @@
-//const ENDPOINT = ""
+
 
 function fetchCountries(name){
-   return fetch(`https://restcountries.com/v2/name/${name}?fields=name,flags,capital,languages,population`)
+   return fetch(`https://restcountries.com/v3/name/${name}?fields=name,flags,capital,languages,population`)
   .then((response) => {
     return response.json();
   })
-//   .then((country) => {
-//     console.log(country);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-//     console.log(name);
 }
 export default {fetchCountries};
--- src/index.js
+++ src/index.js
@@ -1,75 +1,85 @@
 import './css/styles.css';
 import API from './fetchCountries.js';
 import debounce from 'lodash.debounce';
-import notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 
 const input = document.getElementById('search-box')
 const listEl = document.querySelector('.country-list')
 const infoEl = document.querySelector('.country-info')
-
 const DEBOUNCE_DELAY = 300;
 
 input.addEventListener('input', debounce(function (e) {
   e.preventDefault()
   const inputValue = e.target.value.trim();
 
+clear();
 
   API.fetchCountries(inputValue)
-  .then((countries) => {
+  .then((countries) =>  {
   
-    if (countries.length === 404) { 
-      Notiflix.Notify.failure("Oops, there is no country with that name")
+    if (countries.length === 0) { 
+      Notiflix.Notify.failure("Oops, there is no country with that name");
+    } 
 
-    } else if(countries.length > 10){
-      Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+    if(countries.length > 10){
+      Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+    }
 
-    } else if(countries.length >= 2 && countries.length <= 10){
-      return countries.reduce((markup, country) => 
-      markupCountryList(country) + markup,
-      ""
-      )}  else if (countries.length === 1){
-    return countries.reduce((markup, country) =>
-    markupCountryInfo(country),
-      ""
-      )}
+    if(countries.length >= 2 && countries.length <= 10){
+      const markup = countries.map(country => markupCountryList(country)).join("")
       
-  }).then(updateList)
-  .then(updateInfo) 
-  
+      listEl.innerHTML = markup;
+     
+    }  
+      if (countries.length === 1){
+        const markup = countries.map(country => markupCountryInfo(country)).join("")
+        infoEl.innerHTML = markup;
+      
+    }
+    
 }, DEBOUNCE_DELAY)
-)
-
-
+}
+)),
 
 function markupCountryList({name, flags, capital}){
-  return `
-  <h2>Country: ${name}</h2>
-  <img src=${flags.svg} alt=":${name} width="70" heigth="100>
-  <p>Capital: ${capital}</p>
-  `
-}
 
-function updateList(markup){
-  document.querySelector('.country-list').innerHTML = markup;
-}
+  return  `<li>
+  <h2>Country: ${name.official}</h2>
+  <img src="${flags.svg}" alt="${name.official}" width="70" heigth="50">
+  <p>Capital: ${capital}</p>
+  </li>`
+},
 
 function markupCountryInfo({name, flags, capital, languages, population}){
   const lang = languages.map(lang => lang.name).join(", ")
 
-  return `
-  <h2>Country: ${name}</h2>
-  <img src=${flags.svg} alt=":${name}" width="70" heigth="100">
+  return `<li>
+  <h2>Country: ${name.official}</h2>
+  <img src="${flags.svg}" alt="${name.official}" width="70" heigth="50">
   <p>Capital: ${capital}</p>
   <p>Languages: ${lang}</p>
   <p>Population: ${population}</p>
-  `
+  </li>`
+};
+
+function clear(){
+  listEl.innerHTML = "";
+  infoEl.innerHTML = "";
 }
 
-function updateInfo(markup){
-  document.querySelector('.country-info').innerHTML = markup;
-}
+
+
+
+// function updateList(markup){
+//   listEl.innerHTML = markup;
+// },
+
+
+// function updateInfo(markup){
+//   infoEl.innerHTML = markup;
+// };
 
 
 
 
-//.then(updateInfo) // (abo .then((markup)=>updateInfo(markup)) (te same))
+//.then(updateInfo) // (abo .then((markup)=>updateInfo(markup)) (te same)
